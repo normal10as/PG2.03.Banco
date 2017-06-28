21d8df339c41d4b2261c59037f6f8cc8e8c81347
Fabio Dos Santos <fabio2st@yahoo.com>
2017-06-28 20:14:16 -0300

    Add project files.

---
+++ BancoEntidades/Cliente.vb
@@ -0,0 +1,13 @@
+﻿Public Class Cliente
+    Private _nombre As String
+    Private _documento As Integer
+    Private _fechaNacimiento As Date
+
+    Sub New()
+        _nombre = ""
+        _documento = 0
+        _fechaNacimiento = Nothing
+    End Sub
+
+
+End Class
---
+++ BancoTest/ClienteTest.vb
@@ -0,0 +1,13 @@
+﻿Imports BancoEntidades
+
+Module ClienteTest
+
+    Sub Main()
+        ' declaraciòn
+        Dim cliente1 As Cliente
+        ' instanciacion
+        cliente1 = New Cliente
+        'cliente1.
+    End Sub
+
+End Module
